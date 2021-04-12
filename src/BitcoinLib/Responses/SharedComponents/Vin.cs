44ba67c6b959289cdec3226289ebfd1f90f77936
human-guy <donatas.skaito@gmail.com>
2021-04-12 16:44:52 -0400

    Added txinwitness

--- src/BitcoinLib/Responses/SharedComponents/Vin.cs
+++ src/BitcoinLib/Responses/SharedComponents/Vin.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) 2014 - 2016 George Kimionis
 // See the accompanying file LICENSE for the Software License Aggrement
 
+using System.Collections.Generic;
+
 namespace BitcoinLib.Responses.SharedComponents
 {
     public class Vin
@@ -10,6 +12,7 @@ namespace BitcoinLib.Responses.SharedComponents
         public ScriptSig ScriptSig { get; set; }
         public string CoinBase { get; set; }
         public string Sequence { get; set; }
+        public List<string> TXinWitness { get; set; }
     }
 
     public class ScriptSig
